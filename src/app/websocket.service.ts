bc924abb65367cfe0b2668150a302487ef6cf7d7
Riley Littlefield <rileylittlefield6122428@gmail.com>
2023-02-18 23:07:56 -0500

    checkpoint

---
+++ src/app/websocket.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { catchError, EMPTY, Subject, tap } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+const WS_ENDPOINT = 'ws://127.0.0.1:8000/ws/chat/room1/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  private socket$: WebSocketSubject<any> | null = null;
+  public messages$ = new Subject<any>();
+
+  public connect(): void {
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = this.getNewWebSocket();
+      this.socket$.pipe(
+        tap({ error: error => console.log(error) }),
+        catchError(_ => EMPTY)
+      )
+        .subscribe((messages) => {
+          this.messages$.next(messages);
+        });
+    }
+  }
+
+  private getNewWebSocket() {
+    return webSocket(WS_ENDPOINT);
+  }
+
+  sendMessage(message: any) {
+    this.socket$!.next({ message });
+  }
+
+  close() {
+    this.socket$!.complete();
+  }
+}
